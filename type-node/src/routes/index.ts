f8bbbd48db6f7c0a3fe5e5622a9fc0b584aca78b
bivarz <paulo.bivar@gmail.com>
2020-07-15 23:29:32 -0300

    addingRoutes

--- type-node/src/routes/index.ts
+++ type-node/src/routes/index.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
+routes.use('/appointments', appointmentsRouter);
 export default routes;
