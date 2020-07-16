adf0b97724c3a530f2711747ba06eb6758cf1251
bivarz <paulo.bivar@gmail.com>
2020-07-16 18:43:32 -0300

    addingRoutes

--- type-node/src/routes/appointments.routes.ts
+++ type-node/src/routes/appointments.routes.ts
@@ -2,4 +2,10 @@ import { Router } from 'express';
 
 const appointmentsRouter = Router();
 
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  return response.json({ message: 'hello' });
+});
+
 export default appointmentsRouter;
